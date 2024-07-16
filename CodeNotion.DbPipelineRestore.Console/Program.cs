c87b3fce38a10d4eff2f61a110012a612e520c81
Dario <dario.ferrai>
2024-07-16 12:51:00 +0300

    Re organizing

---
+++ CodeNotion.DbPipelineRestore.Console/Program.cs
@@ -0,0 +1,8 @@
+﻿using CodeNotion.DbPipelineRestore;
+
+var restorer = new DbPipelineRestorer();
+
+const string source = "Server=127.0.0.1,1433;Database=DrSafety;uid=sa;pwd=yourStrong(!)Password;MultipleActiveResultSets=True;TrustServerCertificate=True";
+const string target = "Server=127.0.0.1,1433;Database=DrSafety2;uid=sa;pwd=yourStrong(!)Password;MultipleActiveResultSets=True;TrustServerCertificate=True";
+
+await restorer.Restore(source, target, "DrSafety", "DrSafety2");
---
+++ CodeNotion.DbPipelineRestore/DbPipelineRestorer.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Data.SqlClient;
+using Microsoft.SqlServer.Dac;
+
+namespace CodeNotion.DbPipelineRestore;
+
+public class DbPipelineRestorer
+{
+    public virtual async Task Restore(
+        string sourceConnectionString,
+        string targetConnectionString,
+        string sourceDatabaseName,
+        string targetDatabaseName)
+    {
+        // Create a connection string for your SQL Server
+        var sourceDb = new SqlConnectionStringBuilder
+        {
+            ConnectionString = sourceConnectionString
+        };
+
+        var targetDb = new SqlConnectionStringBuilder
+        {
+            ConnectionString = targetConnectionString
+        };
+
+        // Initialize DacServices
+        var ds = new DacServices(sourceDb.ConnectionString);
+
+        // Export the database to a .bacpac file
+        ds.ExportBacpac(@"./backup.bacpac", sourceDatabaseName);
+
+        // Import the .bacpac file to the target database
+        var package = BacPackage.Load(@"./backup.bacpac");
+
+        var service = new SimpleDeleteTableStrategy();
+        await service.CycleDelete(targetDb.ConnectionString, targetDatabaseName);
+
+        ds.ImportBacpac(package, "DrSafety2");
+    }
+}
